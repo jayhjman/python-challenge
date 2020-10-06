ec848acd7bf196f57da3f055a60c1ac3add8338f
Jay Hastings <jayhjman@gmail.com>
2020-10-06 12:26:31 -0700

    changed to reading the entire data set into list/dictionary

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,11 +18,8 @@ source_path = os.path.dirname(os.path.abspath(__file__))
 # Create file path and name to read
 file_path = os.path.join(source_path, 'Resources', 'budget_data.csv')
 
-# Count the number of months in the file
-number_of_months = 0
-
-# Sum the total amount of profit/losses
-total_amount = 0
+# List to hold the csv data in memory
+bank_profits = []
 
 # Average change
 average_change = 0
@@ -42,15 +39,24 @@ with open(file_path) as csvfile:
 
     # Read each row of file
     for row in csvreader:
-        number_of_months += 1
-        total_amount += int(row[1])
+        profit = {}
+        profit["date"] = row[0]
+        profit["profit"] = int(row[1])
+        bank_profits.append(profit)
+
+# Sum the total amount of profit/losses
+total_amount = 0
+
+# Loop through bank profits calculating values
+for i in range(len(bank_profits)):
+    total_amount += bank_profits[i]["profit"]
 
 
 print('Financial Analysis')
 print('--------------------------------------------------------')
-print(f'Total Months: {number_of_months}')
+print(f'Total Months: {len(bank_profits)}')
 print(f'Total: ${total_amount}')
 print(f'Average  Change: ${average_change}')
 print(f'Greatest Increase in Profits: <Mth-Yr> $({greatest_increase})')
-print(f'Greatest Decrease in Profits: <Mth-Yr> g$({greatest_decrease})')
+print(f'Greatest Decrease in Profits: <Mth-Yr> $({greatest_decrease})')
 print('--------------------------------------------------------')
