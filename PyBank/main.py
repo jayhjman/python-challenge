b00186122c5c7bbcf30c1c13488ebbf40b2a2b01
Jay Hastings <jayhjman@gmail.com>
2020-10-06 11:25:01 -0700

    added loop for reading in the csv file, also added .gitignore for .vscode/

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,7 @@
 
 # Imports
 import os
-
+import csv
 
 # Find the path of the current file
 source_path = os.path.dirname(os.path.abspath(__file__))
@@ -18,3 +18,18 @@ source_path = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(source_path, 'Resources', 'budget_data.csv')
 
 print(f'file_path: {file_path}')
+
+with open(file_path) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
