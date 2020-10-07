e9026919a2c975b5a16459bf4ff2eba6ff87263d
Jay Hastings <jayhjman@gmail.com>
2020-10-06 18:33:26 -0700

    added output logic, string formats, and winner calcualtion

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -50,16 +50,44 @@ def proccess_voters(voters):
             voter_summary[candidate] = {"votes": 1}
 
     # Loop through to get vote %
+    winner = ""
+    vote_count = 0
     for candidate in voter_summary.keys():
         votes = voter_summary[candidate]["votes"]
+        if votes > vote_count:
+            vote_count = votes
+            winner = candidate
         percent_votes = votes / len(voters) * 100
         voter_summary[candidate].update({"percent_votes": percent_votes})
 
     voter_summary = {"candidates": voter_summary}
 
     voter_summary.update({"total_votes": len(voters)})
+    voter_summary.update({"winner": winner})
 
     return voter_summary
+
+
+def output_results(output_file, summary):
+
+    string_to_output = 'Election Results\n'
+    string_to_output += '-------------------------------\n'
+    string_to_output += str.format(f'Total Votes: {summary["total_votes"]}\n')
+    string_to_output += '-------------------------------\n'
+    candidates = summary["candidates"]
+    for candidate in candidates:
+        candidate_info = candidates[candidate]
+        string_to_output += str.format(
+            f'{candidate}: {candidate_info["percent_votes"]:.3f}% ({candidate_info["votes"]})\n')
+    string_to_output += '-------------------------------\n'
+
+    # Print out the summary table
+    print(string_to_output)
+
+    # Write the output results text file
+    results_file = open(output_file, 'w')
+    num_of_chars = results_file.write(string_to_output)
+    results_file.close()
 
 
 def main():
@@ -73,11 +101,9 @@ def main():
 
     voters_list = read_voters_from_csv(read_file)
 
-    print(len(voters_list))
-
     summary_of_votes = proccess_voters(voters_list)
 
-    print(summary_of_votes)
+    output_results(write_file, summary_of_votes)
 
 
 if __name__ == "__main__":
