8236301b7edda77e400aaa5f4283e2f18eb0b144
Jay Hastings <jayhjman@gmail.com>
2020-10-06 18:57:30 -0700

    added additional commenting and cleaned some code up

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,20 @@
 #
 # PyPoll
 #
-# Script that analyzes the financial records of the given bank.
-# The purpose is show how many months the analysis covers, the
-# total amount profit/losses, average change, greatest increase,
-# and greatest decrease.
-#
+# Script that analyzes the election records for the given candidates.
+# The purpose is show who won the election, the percentage and total
+# breakdown of votes for each candidate, and final overall total vote
+# count.
 
 # Imports
 import os
 import csv
+
+#
+# read_voters_from_csv
+#
+# Read the file from the csv file and populate the voters list then
+# return the voters list back to the caller
 
 
 def read_voters_from_csv(input_file):
@@ -36,6 +41,15 @@ def read_voters_from_csv(input_file):
 
     return voters
 
+#
+# process_voters
+#
+# Take the election voter list and find
+#    1. Total number of votes
+#    2. Total amount of votes for each candidate
+#    3. The percentage of votes each candidate received
+#    4. Determine the winner with the highest number of votes
+
 
 def proccess_voters(voters):
 
@@ -49,17 +63,19 @@ def proccess_voters(voters):
         else:
             voter_summary[candidate] = {"votes": 1}
 
-    # Loop through to get vote %
+    # Loop through to get vote % and the winner with highest count
     winner = ""
     vote_count = 0
-    for candidate in voter_summary.keys():
+    for candidate in voter_summary:
         votes = voter_summary[candidate]["votes"]
+        # if latest candidate has higher vote than last, could be the winner!
         if votes > vote_count:
             vote_count = votes
             winner = candidate
         percent_votes = votes / len(voters) * 100
         voter_summary[candidate].update({"percent_votes": percent_votes})
 
+    # Make a nice dict with all the results in 1 package
     voter_summary = {"candidates": voter_summary}
 
     voter_summary.update({"total_votes": len(voters)})
@@ -68,18 +84,30 @@ def proccess_voters(voters):
     return voter_summary
 
 
+#
+# output_results
+#
+# Output to console and file the summary passed by formatting it to text
+
 def output_results(output_file, summary):
 
+    # Format the string for the election results
     string_to_output = 'Election Results\n'
     string_to_output += '-------------------------------\n'
     string_to_output += str.format(f'Total Votes: {summary["total_votes"]}\n')
     string_to_output += '-------------------------------\n'
+
+    # Loop through candidates to get detailed info on them
     candidates = summary["candidates"]
     for candidate in candidates:
         candidate_info = candidates[candidate]
         string_to_output += str.format(
             f'{candidate}: {candidate_info["percent_votes"]:.3f}% ({candidate_info["votes"]})\n')
+
+    # Winner, Winner, Chicken Dinner
     string_to_output += '-------------------------------\n'
+    string_to_output += str.format(f'Winner: {summary["winner"]}\n')
+    string_to_output += '-------------------------------'
 
     # Print out the summary table
     print(string_to_output)
@@ -88,6 +116,11 @@ def output_results(output_file, summary):
     results_file = open(output_file, 'w')
     num_of_chars = results_file.write(string_to_output)
     results_file.close()
+
+#
+# main
+#
+# main function for executing the program
 
 
 def main():
@@ -99,10 +132,13 @@ def main():
     read_file = os.path.join(source_path, 'Resources', 'election_data.csv')
     write_file = os.path.join(source_path, 'analysis', 'results.txt')
 
+    # Get the list of voters from the csv file
     voters_list = read_voters_from_csv(read_file)
 
+    # Get the summary breakdown of votes, %, totals, and winner
     summary_of_votes = proccess_voters(voters_list)
 
+    # Print out into the console and file the results
     output_results(write_file, summary_of_votes)
 
 
