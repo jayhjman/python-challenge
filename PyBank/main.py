7dbd8a86e21349cf7d57a8adec2bb32d3cffbc54
Jay Hastings <jayhjman@gmail.com>
2020-10-08 11:11:46 -0700

    refactored to avoid loaded data into memory

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,14 +13,40 @@ import csv
 
 
 #
-# read_profits_from_csv
+# process_profits_from_csv
 #
-# Read the file from the csv file and populate the profit list then
-# return the profit list back to the caller
+# Read the rows from the csv file and as you go through them to calculate:
+#
+#    1. Total months being processed
+#    2. Total amount profit over all the months processed
+#    3. Average change in profit month over month
+#    4. Greatest increase in profit from month to month
+#    5. Greatest decrease in profit from month to month
+#
+# Algorithm processes by keeping previous months data and doing calculations
+# using the current month.
+#
 
-def read_profits_from_csv(input_file):
+def process_profits_from_csv(input_file):
 
-    profits = []
+    # Zero out totals for calculations
+    total_months = 0
+    total_amount = 0
+
+    # Total of profit change month over month, used later to calculate average
+    total_profit_change = 0
+
+    # Placeholder for the greatest profit increase
+    greatest_increase = {
+        "date": "",
+        "value": 0
+    }
+
+    # Placeholder for greatest profit decrease
+    greatest_decrease = {
+        "date": "",
+        "value": 0
+    }
 
     # Open csv file for reading
     with open(input_file) as csvfile:
@@ -31,90 +57,53 @@ def read_profits_from_csv(input_file):
         # Skip header row first as there is a header in the file
         csv_header = next(csvreader)
 
+        # Create an empty previous row to start
+        previous_row = {}
+
         # Read each row of file append to our list to be processed later
         for row in csvreader:
-            profit = {
+
+            # Get total number of months and profit amounts
+            total_months += 1
+            total_amount += int(row[1])
+
+            # Start doing the calculations after previous row is set (skips first row)
+            if bool(previous_row):
+
+                # profit change need for greatest calculation and total profits needed for average
+                profit_change = int(row[1]) - previous_row["value"]
+                total_profit_change += profit_change
+
+                # Track the greatest increase between months
+                if profit_change > greatest_increase["value"]:
+                    greatest_increase = {
+                        "date": row[0],
+                        "value": profit_change
+                    }
+
+                # Track the greatest decrease between months
+                if profit_change < greatest_decrease["value"]:
+                    greatest_decrease = {
+                        "date": row[0],
+                        "value": profit_change
+                    }
+
+            # Track previous row for month to month calculations
+            previous_row = {
                 "date": row[0],
-                "profit": int(row[1])
-            }
-            profits.append(profit)
-
-    return profits
-
-
-#
-# process_profits
-#
-# Take the bank profit list and calculate
-#    1. Total months being processed
-#    2. Total amount over all the months processed
-#    3. Average change in profile month over month
-#    4. Greatest increase in profit from month to month
-#    5. Greatest decrease in profit from month to month
-
-
-def process_profits(profits):
-
-    # Sum the total amount of profit/losses
-    total_amount = 0
-
-    # Average change month over month
-    average_change = 0
-
-    # Greatest increase and decrease month over month
-    greatest_increase = {
-        "index": 0,
-        "value": 0
-    }
-    greatest_decrease = {
-        "index": 0,
-        "value": 0
-    }
-
-    # Keep a rolling total of the average change month over month
-    average_total = 0
-
-    # Loop through bank profits calculating values (stopping 1 before the end to avoid index out of range issues)
-    for i in range(len(profits)-1):
-
-        # Count up totals for later calculations
-        total_amount += profits[i]["profit"]
-        profit_change = profits[i+1]["profit"] - profits[i]["profit"]
-        average_total += profit_change
-
-        # Track the greatest increase between months
-        if profit_change > greatest_increase["value"]:
-            greatest_increase = {
-                "value": profit_change,
-                "index": i + 1
+                "value": int(row[1])
             }
 
-        # Track the greatest decrease between months
-        if profit_change < greatest_decrease["value"]:
-            greatest_decrease = {
-                "value": profit_change,
-                "index": i + 1
-            }
-
-    # We stopped on short in the loop above, make sure to add final value
-    total_amount += profits[len(profits)-1]["profit"]
-
-    # Change will be 1 less value than months since month to month comparision
-    average_change = average_total/(len(profits)-1)
-
-    # Build the dictionary for the summary
-    bank_info = {
-        "total_months": len(profits),
+    # Build a summary dict for display purposes to return to caller
+    return {
+        "total_months": total_months,
         "total_amount": total_amount,
-        "average_change": average_change,
-        "greatest_increase_month": profits[greatest_increase["index"]]["date"],
+        "average_change": (total_profit_change/(total_months - 1)),
+        "greatest_increase_month": greatest_increase["date"],
         "greatest_increase_value": greatest_increase["value"],
-        "greatest_decrease_month": profits[greatest_decrease["index"]]["date"],
+        "greatest_decrease_month": greatest_decrease["date"],
         "greatest_decrease_value": greatest_decrease["value"],
     }
-
-    # Return the bank info
-    return bank_info
 
 
 #
@@ -161,15 +150,13 @@ def main():
     read_file = os.path.join(source_path, 'Resources', 'budget_data.csv')
     write_file = os.path.join(source_path, 'analysis', 'results.txt')
 
-    # List to hold the csv data in memory
-    bank_profits = read_profits_from_csv(read_file)
-
-    # Process the bank profit list and return the summary values
-    bank_info = process_profits(bank_profits)
+    # Read and process data from the cvs file, could be rather large so process as you read
+    bank_info = process_profits_from_csv(read_file)
 
     # Output to both console and file
     output_results(write_file, bank_info)
 
 
+# Call the main function
 if __name__ == "__main__":
     main()
